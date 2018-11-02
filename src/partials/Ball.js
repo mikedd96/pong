5944fa354ed293255b4ff7e16eb74d65e151b260
mike <mike_4596@hotmail.com>
2018-11-02 09:46:30 -0700

    ball moves

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -5,12 +5,30 @@ export default class Ball {
       this.boardWidth = boardWidth;
       this.boardHeight = boardHeight;
       this.direction = 1;
+      this.reset()
+      
     }
 
+    reset() {
+        this.x = this.boardWidth / 2;
+        this.y = this.boardHeight / 2;
+       
+        this.vy = 0;
+        while( this.vy === 0){
+            this.vy = Math.floor(Math.random() * 10 - 5);
+        }
+        this.vx = this.direction * (6 - Math.abs(this.vy))
+        
+      }
+
     render(svg){
+
+        this.x += this.vx;
+        this.y += this.vy;
+
         let ball = document.createElementNS(SVG_NS, 'circle');
-        ball.setAttributeNS(null, 'cx', (this.boardWidth/2));
-        ball.setAttributeNS(null, 'cy', (this.boardHeight/2));
+        ball.setAttributeNS(null, 'cx', this.x);
+        ball.setAttributeNS(null, 'cy', this.y);
         ball.setAttributeNS(null, 'r', this.radius);
         ball.setAttributeNS(null, 'fill', '#025ed4');
 
--- src/partials/Game.js
+++ src/partials/Game.js
@@ -19,6 +19,7 @@ export default class Game {
 		this.boardHeight = 256;
 		this.radius = 8;
 		this.direction = 1;
+		
 
 		this.ball = new Ball(
 			this.radius,
